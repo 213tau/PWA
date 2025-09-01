c6b302a7a8933f371b6556488b227b351dc8bdce
213tau <213tauseef@gmail.com>
2025-09-01 23:06:22 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,13 +7,33 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker activated.');
 });
 
-self.addEventListener('fetch', function (event) {
+const CACHE_NAME = "my-pwa-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/service-worker.js",
+  "/script.js",
+  "/share-target.html",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+];
+
+// Install event: cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch event: serve cached files if offline
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // If cached, return it; otherwise fetch from network
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
 });
+
+
 
 
